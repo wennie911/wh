a56ee747f77c8fa71accae9a9ac781024dd57a49
wennie911 <41497800+wennie911@users.noreply.github.com>
2018-09-06 11:00:59 +0800

    有问题删除

--- aiesl/loadconfig.h
+++
@@ -1,7 +0,0 @@
-#ifndef _H_LOAD_CONFIG
-#define _H_LOAD_CONFIG
-
-int GetProfileString(char *profile, char *AppName, char *KeyName, char *KeyVal);
-int GetProfileInt(char *profile, char *AppName, char *KeyName, int *KeyVal);
-
-#endif
